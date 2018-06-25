88e08112bbb52570af099e7c8a9771faa30fcf34
ritztoston <ritztoston@gmail.com>
2018-06-25 10:00:10 +0800

    Create profile

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:Kimritzter1@ds217131.mlab.com:17131/devconnector'
+  mongoURI: 'mongodb://admin:Kimritzter1@ds217131.mlab.com:17131/devconnector',
+  secretOrKey: 'secret'
 };
